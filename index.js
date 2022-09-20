0f5490017f16b8f9439615df8b6c259dd640c7e1
IvanGarbi <63320947+IvanGarbi@users.noreply.github.com>
2022-09-20 20:59:58 -0300

    Commitando o TDE

---
+++ index.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import Blog from "./model/Blog.js";
+
+mongoose.connect("mongodb+srv://TDEBancoDados:mongo@cluster0.6q4rkvi.mongodb.net/?retryWrites=true&w=majority");
+
+// // Create a new blog post object
+// const article = new Blog({
+//     title: "Awesome Post!",
+//     slug: "awesome-post",
+//     published: true,
+//     content: "This is the best post ever",
+//     tags: ["featured", "announcement"],
+// });
+
+// // Insert the article in our MongoDb database
+// await article.save();
+
+// // Find a single post
+// const firstArticle = await Blog.findOne({});
+// console.log(firstArticle);
+
+const article = Blog.create({
+    title: "Awesome Post!",
+    slug: "awesome-post",
+    published: true,
+    content: "This is the best post ever",
+    tags: ["featured", "announcement"],
+});
+
+const firstArticle = await Blog.findOne({});
+
+console.log(firstArticle);
